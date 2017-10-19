be97807dff862ec449fe4df7db81975001c1356b
victor <victor.rojas@glr.pe>
2017-10-19 12:36:37 -0500

    cambio

--- js/muestras.js
+++ js/muestras.js
@@ -8,6 +8,12 @@ $(document).ready(function(){
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
+
+		var res = tab_id.split("-");
+		//console.log("#"+tab_id+' #slides'+res[1]+' .slidesjs-container');
+		$("#"+tab_id+' #slides'+res[1]+' .slidesjs-container').css("width", "836");
+		$("#"+tab_id+' #slides'+res[1]+' .slidesjs-container').css("height", "469.583");
+
 	})
 
 })
